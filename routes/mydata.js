4c8d01eb74f3d38f55f7f2f2556bf424e765023e
uma0441 <89418434+uma0441@users.noreply.github.com>
2021-10-27 02:54:47 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('/mydata', { title: 'Manikanta Vasana' });
+  res.render('mydata', { title: 'Manikanta Vasana' });
 });
 
 module.exports = router;
