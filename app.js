ac4cdf4f0baf22a109ce6e0aa9d76dfed93f9bfa
uma0441 <89418434+uma0441@users.noreply.github.com>
2021-10-27 02:31:15 -0500

    New Endpoint

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mydataRouter = require('./routes/mydata');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mydataRouter = require('./routes/mydata');
 
 var app = express();
 
@@ -19,8 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/mydata', mydataRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', mydataRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
